306ad10272e321c6cfdb09ceef47dac20cc58411
Rev Dan Catt <revdancatt@gmail.com>
2012-03-10 12:52:37 +0000

    Here's the code to drop in the initial set of images and then fetch them once a minute after that

---
+++ js/control.js
@@ -0,0 +1,123 @@
+control = {
+    
+    apiUrlHash: {},
+    reloadTmr: null,
+    imgWidth: 460,
+    imgHeight: 276,
+    resizeTmr: null,
+
+    init: function() {
+
+        var fit = parseInt($('body').width()/460, 10)+1;
+        control.imgWidth = parseInt($('body').width()/fit, 10);
+        control.imgHeight = parseInt(276 * control.imgWidth/460, 10);
+
+        this.fetchStories(60, true);
+        this.reloadTmr = setInterval("control.fetchStories(10, false)", 60000);
+
+        $(window).resize(function() {
+            utils.windowResized();
+        });
+
+    },
+
+    fetchStories: function(limit, firstrun) {
+
+        var url = '/api/getLatest';
+
+        $.getJSON(encodeURI(url),
+            function(json) {
+
+                var results = json.results.storiesList;
+                if (!firstrun) {
+                    results = results.reverse();
+                }
+
+                //  go thru the results
+                for (var i in results) {
+
+                    //  check to see if we already have it
+                    if (!(results[i] in control.apiUrlHash)) {
+                        control.apiUrlHash[results[i].apiUrl] = -1;
+
+                        var story = json.results.stories[results[i]];
+
+                        //  The main div that's going to hold the front and back of the story 'card'
+                        var d = $('<div>').addClass('container').css({'width': control.imgWidth, 'height': control.imgHeight});
+                        var c = $('<div>').addClass('card');
+
+                        d.mouseenter(function() {
+                            control.temp = $(this);
+                            $(this).find('.back').stop(true, true).fadeIn(333);
+                        });
+                        d.mouseleave(function() {
+                            $(this).find('.back').stop(true, true).fadeOut(666);
+                        });
+
+                        d.data('apiUrl', story.apiUrl);
+
+                        //  The front, which holds the picture
+                        var front = $('<div>').addClass('face front');
+                        var img = $('<img>').addClass('bigImage').attr('src', story.imgUrl);
+                        img.load( function() {
+                            $(this).parents('.container').fadeTo(666, 1);
+                        });
+
+
+                        //  Now we need the back that has the information on it
+                        var back = $('<div>').addClass('face back');
+                        back.append($('<h1>').html(story.sectionName));
+                        back.append($('<h2>').html(story.webTitle));
+
+                        if ('imgCaption' in story) {
+                            back.append($('<div>').addClass('photocredit').append($('<p>').html('Image credit: ' + story.imgCaption)));
+                        }
+
+                        front.append(img);
+                        c.append(front);
+                        c.append(back);
+
+                        var a = $('<a>').attr({'href': story.webUrl, 'target': '_blank'});
+                        a.append(c);
+                        d.append(a);
+
+                        $('#bigImageHolder').prepend(d);
+
+                    }
+                }
+
+                //  trim off any more than 60 results
+                while ($('div.bigImage').length > 60) {
+                    var apiUrl = $('div.bigImage').last().data('apiUrl');
+                    if (apiUrl in control.apiUrlHash) {
+                        delete control.apiUrlHash[apiUrl];
+                    }
+                    $('div.bigImage').last().remove();
+                }
+
+            }
+        );
+
+    }
+};
+
+
+utils = {
+    
+    log: function(msg) {
+        try {
+            console.log(msg);
+        } catch(er) {
+        }
+    },
+
+    windowResized: function() {
+
+        var fit = parseInt($('body').width()/460, 10)+1;
+        control.imgWidth = parseInt($('body').width()/fit, 10);
+        control.imgHeight = parseInt(276 * control.imgWidth/460, 10);
+        $('div.container').css({'width': control.imgWidth, 'height': control.imgHeight});
+
+    }
+
+};
