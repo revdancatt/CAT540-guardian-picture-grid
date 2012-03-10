158b6c8e02e8457652c7ed394390cc3f8151738f
Rev Dan Catt <revdancatt@gmail.com>
2012-03-10 12:36:08 +0000

    Allow you to specify a limit on the call to get latest, so we don't need to grab all 60 once a minute :)

--- api/getLatest.py
+++ api/getLatest.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+import os
+import sys
 import simplejson
 from admin.models import Settings
 from google.appengine.api import memcache
+from admin.functions import get_query_string
+
+args = get_query_string(os.environ['QUERY_STRING'])
 
 memkey = 'settings'
 
@@ -16,6 +21,18 @@ else:
     results = simplejson.loads(itemsRows[0].json)
     del results['rejects']
     del results['rejectsList']
+
+    #   TODO: validate limit in args as positive numerical
+    if 'limit' in args:
+        newStories = {}
+        newStoriesList = []
+        for i in range(int(args['limit'])):
+            newStories[results['storiesList'][i]] = results['stories'][results['storiesList'][i]]
+            newStoriesList.append(results['storiesList'][i])
+
+        results['stories'] = newStories
+        results['storiesList'] = newStoriesList
+
     newResults = {'stat': 'ok', 'results': results}
 
 print 'Cache-Control: public,max-age=60'
